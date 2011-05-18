cb6e1fb30b2cf140795cc5c58a0ddaa038de7266
Swizec Teller <swizec@swizec.com>
2011-05-18 16:11:32 +0200

    improved tests, support for adding shortener API's

--- test/unshortener.test.js
+++ test/unshortener.test.js
@@ -4,18 +4,48 @@ var unshortener = require('../lib/unshortener')
 var urllib = require('url');
 
 
+var unshort = unshortener.Unshorteners;
+
 module.exports = {
-    'expand_string': function () {
-        unshortener.expand('http://bit.ly/mShvIR', function (url) {
-            assert.strictEqual(url.href, 'http://www.premiumpixels.com/freebies/add-to-cart-buttons-psd/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+premiumpixels+%28Premium+Pixels%29');
+    'expand_string': function (beforeExit) {
+	var fired = false;
+
+        unshortener.expand('http://is.gd/E27w2x', function (url) {
+            assert.equal(url.href,
+			 'http://holykaw.alltop.com/why-sleep-may-be-the-best-tool-for-tough-deci?tu3=1');
+	    fired = true;
         });
+
+	beforeExit(function () {
+	    assert.equal(fired, true);
+	});
     },
 
-    'expand_object': function () {
+    'expand_object': function (beforeExit) {
         var fired = false;
         unshortener.expand(urllib.parse('http://t.co/rWP6BP3'),
                            function (url) {
-                               assert.strictEqual(url.href, 'http://www.facebook.com/mybrucebruce');
+                               assert.equal(url.href,
+					    'http://www.facebook.com/mybrucebruce');
+			       fired = true;
                            });
+
+	beforeExit(function () {
+	    assert.equal(fired, true);
+	});
+    },
+
+    'expand_bitly': function (beforeExit) {
+	var fired = false;
+	unshort.bitly(urllib.parse('http://bit.ly/lyQusq'),
+		      function (url) {
+			  assert.equal(url.href,
+				       'http://www.crunchgear.com/2011/05/18/review-two-speck-ipad-2-cases/?utm_source=twitterfeed&utm_medium=twitter');
+			  fired = true;
+		      });
+
+	beforeExit(function () {
+	    assert.equal(fired, true);
+	});
     }
 };
